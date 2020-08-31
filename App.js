7c75326fcc848466469e96acef0bdc771657fefa
Matheus Suffi <matheus_suffi@live.com>
2020-08-30 22:58:27 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,Linking,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native'
+import WebView from 'react-native-webview'
+const App = () => {
+  return (
+    <WebView
+      source={{ uri: 'https://www.southernfacility.com' }}
+      style={{ marginTop: 40 }}
+    />
+  )
+}
+
+export default App
